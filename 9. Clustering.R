313a1e64272033835b1288b4990d6e561e6e22fd
Hdez_J <jlopezhdez036@gmail.com>
2024-07-16 18:49:46 +1000

    .2

---
+++ 9. Clustering.R
@@ -0,0 +1,14 @@
+
+# Clustering --------------------------------------------------------------
+
+
+# El agrupamiento (clustering/partitioning) se refiere a la segmentación 
+# de un conjunto de elementos en subconjuntos “naturales”
+
+# La partición de redes, conocida como detección de comunidades, constituye una
+# metodología no supervisada para encontrar subconjuntos homogeneos
+
+
+# Se discuten algoritmos, no modelos estadísticos, por lo que no permiten
+# cuantificar la incertidumbre
+
